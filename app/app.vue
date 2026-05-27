e364b8d3e385b3816797ae1a72fdc8b3992d1ac2
Link1515 <terrylin1515@gmail.com>
2026-05-27 16:50:50 +0800

    first commit

---
+++ app/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div style="display: flex; gap: 10px">
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/test">test</NuxtLink>
+  </div>
+
+  <div>
+    <NuxtPage />
+  </div>
+</template>
---
+++ app/pages/test.vue
@@ -0,0 +1,11 @@
+<script setup>
+const flag = ref(false);
+const md = computed(() => (flag.value ? "# flag is true" : "# flag is false"));
+</script>
+
+<template>
+  <div>
+    <button @click="flag = !flag">toggle</button>
+    <MDC :value="md" />
+  </div>
+</template>
